1390afe18b719e89ace1a9c9f9fc6d1f72f886d5
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2022-02-23 01:29:01 -0700

    Refactor score model

--- src/models/score.model.ts
+++ src/models/score.model.ts
@@ -1,6 +1,5 @@
-import { v4 as uuidv4 } from "uuid";
-import { DB } from "../classes/db";
 import { TModel } from "../interfaces/model";
+import { DB } from "../classes/db";
 
 export interface TScore {
   id: string;
@@ -8,29 +7,32 @@ export interface TScore {
   player2_score: number;
   player1_name: string;
   player2_name: string;
-  current_winner: string;
+  currentWinner: string;
   scoreDiff: () => void;
 }
 
-export class Score extends DB implements TScore, TModel<TScore> {
-  id: string;
+export interface TScoreDTO {
   player1_score: number;
   player2_score: number;
   player1_name: string;
   player2_name: string;
-  current_winner: string;
+  currentWinner?: string;
+  scoreDiff?: number;
+}
 
-  constructor(score: TScore) {
-    super();
-    this.id = uuidv4();
-    this.player1_name = score.player1_name;
-    this.player2_name = score.player2_name;
-    this.player1_score = score.player1_score;
-    this.player2_score = score.player2_score;
-    this.current_winner = score.current_winner;
+export class Score extends DB implements TModel<TScore> {
+  constructor() {
+    super("./public/data/collections/scores.json");
   }
 
-  scoreDiff() {
-    return Math.abs(this.player1_score - this.player2_score);
+  public calcDiff(data: TScore | TScoreDTO) {
+    return Math.abs(data.player1_score - data.player2_score);
+  }
+
+  public calcCurrentWinner(data: TScore | TScoreDTO) {
+    if (data.player1_score < data.player2_score) {
+      return data.player2_name;
+    }
+    return data.player1_name;
   }
 }
