30eb4d4d260d3e6bb28d049c27f36c4512d4287b
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2022-02-23 01:29:20 -0700

    Add basic REST services for scores collection

--- src/controllers/score.controller.ts
+++ src/controllers/score.controller.ts
@@ -2,44 +2,52 @@ import { Request, Response } from "express";
 import { HTTP_STATUS_CODES } from "../utils/constants/httpStatusCodes";
 import { INTERNAL_SERVER_ERROR } from "../utils/constants/strings";
 import { response } from "../utils/functions/response";
+import { Score, TScoreDTO } from "../models/score.model";
 
 const { OK, ERROR, CREATED } = HTTP_STATUS_CODES;
 
-export const getScore = (req: Request, res: Response) => {
+export const getScore = async (req: Request, res: Response) => {
+  const { id } = req.params;
   try {
-    return response(res, OK, "", {}, null);
+    const score = new Score();
+    const data = await score.find(id);
+    return response(res, OK, "Queried score", data, null);
   } catch (error) {
     return response(res, ERROR, INTERNAL_SERVER_ERROR, null, error);
   }
 };
 
-export const getScores = (req: Request, res: Response) => {
+export const getScores = async (req: Request, res: Response) => {
   try {
-    return response(res, OK, "", {}, null);
+    const score = new Score();
+    const data = await score.read();
+    return response(res, OK, "Queried scores", data, null);
   } catch (error) {
     return response(res, ERROR, INTERNAL_SERVER_ERROR, null, error);
   }
 };
 
-export const postScore = (req: Request, res: Response) => {
+export const postScore = async (req: Request, res: Response) => {
+  const data: TScoreDTO = req.body;
   try {
-    return response(res, CREATED, "", null, null);
+    const score = new Score();
+    const currentWinner = score.calcCurrentWinner(data);
+    const winDiff = score.calcDiff(data);
+    data.currentWinner = currentWinner;
+    data.scoreDiff = winDiff;
+    const result = await score.save(data);
+    return response(res, CREATED, "Saved score", result, null);
   } catch (error) {
     return response(res, ERROR, INTERNAL_SERVER_ERROR, null, error);
   }
 };
 
-export const putScore = (req: Request, res: Response) => {
+export const deleteScore = async (req: Request, res: Response) => {
+  const { id } = req.params;
   try {
-    return response(res, OK, "", {}, null);
-  } catch (error) {
-    return response(res, ERROR, INTERNAL_SERVER_ERROR, null, error);
-  }
-};
-
-export const deleteScore = (req: Request, res: Response) => {
-  try {
-    return response(res, OK, "", {}, null);
+    const score = new Score();
+    const collection = await score.delete(id);
+    return response(res, OK, "", collection, null);
   } catch (error) {
     return response(res, ERROR, INTERNAL_SERVER_ERROR, null, error);
   }
--- src/routes/score.routes.ts
+++ src/routes/score.routes.ts
@@ -2,17 +2,15 @@ import {
   getScore,
   getScores,
   postScore,
-  putScore,
   deleteScore,
 } from "../controllers/score.controller";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/", getScore);
-router.get("/:id", getScores);
+router.get("/", getScores);
+router.get("/:id", getScore);
 router.post("/", postScore);
-router.put("/:id", putScore);
 router.delete("/:id", deleteScore);
 
 export default router;
